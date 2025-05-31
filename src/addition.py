c69c78db074bcd5738bcb161f508447fee9410c4
omgitspradeep <63057831+omgitspradeep@users.noreply.github.com>
2025-05-31 07:16:20 +0545

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,9 +1,8 @@
 # app.py
-# This is a test commit.
+# This is a test commit
 def add(a, b):
     return a + b
 
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
-    assert add(5, 2) == 7
